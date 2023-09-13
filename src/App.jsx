5f5874b0cd50a93db928a447b01ae8a318e78eef
ummemunia <muniaict@gmail.com>
2023-09-13 10:51:15 +0600

    initialize projects

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="text-2xl mx-4 bg-red-100 text-white">Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
